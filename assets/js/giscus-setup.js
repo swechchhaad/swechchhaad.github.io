1017d49c6c9135f463d42d3b71e009abf0f27041
swechchhaad <swechchha.adhikari4@gmail.com>
2025-12-04 09:04:45 +0000

    Deploying to gh-pages from @ swechchhaad/swechchhaad.github.io@bcdbf76337343ca1e8f32fc008f68609adffb5f3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "swechchhaad/swechchha",
+    "data-repo": "swechchhaad/swechchhaad.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
