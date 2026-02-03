902acfe29e4ed1aed33d3a0e650dce2f8360ac24
swechchhaad <swechchha.adhikari4@gmail.com>
2026-02-03 04:51:10 +0000

    Deploying to gh-pages from @ swechchhaad/swechchhaad.github.io@327e07fe3cd2aeab725ceaf02258772897faf4b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "post-a-post-with-plotly-js",
+  },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
