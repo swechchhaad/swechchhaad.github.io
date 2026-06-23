6084a1f697444a64398d0480e9de336d5ce60460
swechchhaad <swechchha.adhikari4@gmail.com>
2026-06-23 16:09:17 +0000

    Deploying to gh-pages from @ swechchhaad/website@d7d4e8260986a9731d04ca228329b0ca1f8a45fd 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,18 +42,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{
+            },},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
